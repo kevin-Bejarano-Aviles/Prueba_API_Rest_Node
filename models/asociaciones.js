34126b1fa572f21b7900d5e73e65381d61121764
Kevin Aviles <kevin.aviles@arbusta.net>
2022-11-03 13:49:26 -0300

    empezar otra ves

--- models/asociaciones.js
+++
@@ -1,9 +0,0 @@
-/* const UserModel = require('./User');
-const ProductModel = require('./Products')
-ProductModel.belongsTo(UserModel,{foreignKey:"userId"})
-UserModel.hasMany(ProductModel,{foreingKey:"userId"});
-
-module.exports= {
-    UserModel,
-    ProductModel
-} */
